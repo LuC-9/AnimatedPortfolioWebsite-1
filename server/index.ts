df527960044267b9aa906d4139fdbe2f8b772beb
aarsh <68937277+LuC-9@users.noreply.github.com>
2025-02-06 10:38:27 +0000

    commit

--- server/index.ts
+++ server/index.ts
@@ -37,6 +37,7 @@ app.use((req, res, next) => {
 });
 
 (async () => {
+  // Register routes before setting up Vite or serving static files
   const server = registerRoutes(app);
 
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
